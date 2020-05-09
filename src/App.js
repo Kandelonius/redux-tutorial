d7378074496b5ff12e2e264fb7ed47c99a54a8e1
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-08 19:27:32 -0700

    have the post form made up and taking inputs. I will continue to the post request later.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Posts from "./components/Posts";
+import PostsForm from "./components/PostsForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <PostsForm />
+        <hr />
         <Posts />
       </header>
     </div>
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react'
-
+import './PostStyles.css';
 class Posts extends Component {
     constructor(props) {
-        super(props); 
+        super(props);
         this.state = {
             posts: []
         }
     }
-    componentDidMount(){
-    // componentWillMount(){ **considered unsafe
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(res => res.json())
-    .then(data => this.setState({
-        posts: data
-    })); // usind fetch instead of axios
-}
+    componentDidMount() {
+        // componentWillMount(){ **considered unsafe
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res => res.json())
+            .then(data => this.setState({
+                posts: data
+            })); // usind fetch instead of axios
+    }
     render() {
         const postItems = this.state.posts.map(post => (
-            <div key={post.id}>
+            <div className="post" key={post.id}>
                 <h3>{post.title}</h3>
                 <p>{post.body}</p>
             </div>
-        ))
+        ));
         return (
             <div>
                 <h1>Posts added</h1>
