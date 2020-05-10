f48a54b5eddf4b29327d4a5d91f400dc7f56dc42
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-09 18:55:18 -0700

    inplemented redux, the Traversy approach wasn't working so I'm currently trying another one that has me working in index.js instead of app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
+
 import Posts from "./components/Posts";
 import PostsForm from "./components/PostsForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <PostsForm />
-        <hr />
-        <Posts />
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+  render() {
+    return (
+      // <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <PostsForm />
+            <hr />
+          </header>
+          <Posts />
+        </div>
+      // </Provider>
+    );
+  }
 }
 
 export default App;
